68698656ad4092bf035c729f2135660ae534a7f5
mkhalfadel <fadel.mokahal@gmail.com>
2025-09-30 21:48:46 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,31 @@
-const CACHE_NAME = "lockledger-cache-v1";
+const CACHE_NAME = 'lockledger-cache-v1';
 const ASSETS_TO_CACHE = [
-   "/Lock-Ledger/manifest.json",
-   "/Lock-Ledger/icons/manifest-icon-192.maskable.png",
-   "/Lock-Ledger/icons/manifest-icon-512.maskable.png",
+   './manifest.json',
+   './icons/manifest-icon-192.maskable.png',
+   './icons/manifest-icon-512.maskable.png',
 ];
 
-self.addEventListener('install', (event) => self.skipWaiting());
-self.addEventListener('activate', (event) => self.clients.claim());
+self.addEventListener('install', event => {
+   event.waitUntil(
+      caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS_TO_CACHE))
+   );
+});
 
-// Fetch event
-self.addEventListener("fetch", (event) => {
-   if (event.request.mode === "navigate") return; // always fetch index.html from network
+self.addEventListener('activate', event => {
+   event.waitUntil(
+      caches.keys().then(keys =>
+      Promise.all(keys.map(key => key !== CACHE_NAME && caches.delete(key)))
+   )
+);
+});
+
+self.addEventListener('fetch', event => {
+const req = event.request;
+
+// Skip caching for navigation (index.html) to prevent auto-refresh
+if (req.mode === 'navigate') return;
 
 event.respondWith(
-   caches.match(event.request).then((res) => res || fetch(event.request))
+   caches.match(req).then(res => res || fetch(req))
 );
 });
